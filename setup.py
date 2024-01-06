44f9bb7ccc6f51a91b64ae915bad2fe3b844e18a
Lucas Lopes Amorim <lucaslopesamorim@gmail.com>
2024-01-06 14:17:37 -0300

    docs: fix description

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setuptools.setup(
     version="0.1.0",
     author="Lucas Lopes Amorim",
     author_email="lucaslopesamorim@gmail.com",
-    description="Streamlit component that allows you to capture pasted data in an input box",
+    description=("Streamlit component that allows you to paste images from your clipboard into your app with a button "
+                 "click"),
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(where="src"),
